23370017c7580e199afa22abc40baaa4c6cdcb10
BoundlessWay <70361753+BoundlessWay@users.noreply.github.com>
2024-11-30 10:56:07 +0700

    Edit stage_nds

--- Script/nds.sql
+++ Script/nds.sql
@@ -7,64 +7,64 @@ USE NDSDB;
 GO
 
 -- Bảng States: Lưu trữ thông tin về các bang
-CREATE TABLE NDSDB.dbo.States (
+CREATE TABLE NDSDB.dbo.State (
     state_id INT IDENTITY(1,1) PRIMARY KEY,					-- Khóa phụ tự tăng
-    state_code VARCHAR(50) NOT NULL UNIQUE,					-- Mã định danh duy nhất của bang
-    state_name VARCHAR(50) NOT NULL,						-- Tên bang
-    state_abbreviation VARCHAR(50) NOT NULL					-- Tên viết tắt của bang
+    state_code VARCHAR(2) NOT NULL UNIQUE,					-- Mã định danh duy nhất của bang
+    state_name VARCHAR(255) NOT NULL,						-- Tên bang
+    state_abbr VARCHAR(255) NOT NULL						-- Tên viết tắt của bang
 );
 GO
 
 -- Bảng Counties: Lưu trữ thông tin về các quận/huyện
-CREATE TABLE NDSDB.dbo.Counties (
+CREATE TABLE NDSDB.dbo.County (
     county_id INT IDENTITY(1,1) PRIMARY KEY,							-- Khóa phụ tự tăng
-    county_fips VARCHAR(50) UNIQUE NOT NULL,							-- Mã định danh duy nhất cho mỗi quận (FIPS)
-    county_name VARCHAR(50),											-- Tên quận
-    county_ascii VARCHAR(50),											-- Tên quận không dấu
-    county_full VARCHAR(100),											-- Tên đầy đủ của quận
-    state_code VARCHAR(50) NOT NULL,									-- Mã định danh bang, khóa ngoại tham chiếu đến States
-    county_code VARCHAR(50) NOT NULL,                                   -- Mã quận nội bang
-    lat FLOAT,															-- Vĩ độ của quận
-    lng FLOAT,															-- Kinh độ của quận
+    county_fips VARCHAR(5) UNIQUE NOT NULL,								-- Mã định danh duy nhất cho mỗi quận (FIPS)
+    county_name VARCHAR(255),											-- Tên quận
+    county_ascii VARCHAR(255),											-- Tên quận không dấu
+    county_full VARCHAR(255),											-- Tên đầy đủ của quận
+    county_code VARCHAR(3) NOT NULL,									-- Mã quận nội bang
+    lat DECIMAL(9, 6),													-- Vĩ độ của quận
+    long DECIMAL(9, 6),													-- Kinh độ của quận
     population INT,														-- Dân số của quận
     created_date DATETIME DEFAULT GETDATE(),							-- Ngày tạo bản ghi
     updated_date DATETIME DEFAULT GETDATE(),							-- Ngày cập nhật bản ghi
-    FOREIGN KEY (state_code) REFERENCES NDSDB.dbo.States(state_code)	-- Ràng buộc khóa ngoại tới bảng States
+    state_code VARCHAR(2) NOT NULL,										-- Mã định danh bang, khóa ngoại tham chiếu đến States
+    FOREIGN KEY (state_code) REFERENCES NDSDB.dbo.State(state_code)	-- Ràng buộc khóa ngoại tới bảng States
 );
 GO
 
 -- Bảng Category: Lưu trữ các loại chất lượng không khí (AQI Category)
 CREATE TABLE NDSDB.dbo.Category (
-    category_id INT IDENTITY(1,1) PRIMARY KEY,      -- ID tự tăng cho mỗi loại chất lượng không khí
-    category_name VARCHAR(50) NOT NULL,				-- Loại chất lượng không khí (VD: Good, Moderate, etc.)
-    LowerBound INT NULL,                            -- Mức AQI tối thiểu cho loại chất lượng không khí
-    UpperBound INT NULL,                            -- Mức AQI tối đa cho loại chất lượng không khí
-    aqi_color VARCHAR(50) NULL,						-- Màu sắc AQI tương ứng (VD: Green, Yellow, etc.)
-    description VARCHAR(255) NULL					-- Mô tả chất lượng không khí
+    category_id INT IDENTITY(1,1) PRIMARY KEY,			-- ID tự tăng cho mỗi loại chất lượng không khí
+    category_name VARCHAR(255) NOT NULL,				-- Loại chất lượng không khí (VD: Good, Moderate, etc.)
+    lower_bound INT NULL,								-- Mức AQI tối thiểu cho loại chất lượng không khí
+    upper_bound INT NULL,								-- Mức AQI tối đa cho loại chất lượng không khí
+    aqi_color VARCHAR(255) NULL,						-- Màu sắc AQI tương ứng (VD: Green, Yellow, etc.)
+    description VARCHAR(255) NULL						-- Mô tả chất lượng không khí
 );
 GO
 
 
 -- Bảng AQI_Measurements: Lưu trữ các đo lường chất lượng không khí (AQI)
-CREATE TABLE NDSDB.dbo.AQI_Measurements (
+CREATE TABLE NDSDB.dbo.AQI_Measurement (
     measurement_id INT IDENTITY(1,1) PRIMARY KEY,							-- ID tự tăng cho mỗi bản ghi AQI
-    county_fips VARCHAR(50) NOT NULL,										-- Khóa ngoại tham chiếu tới bảng Counties qua county_fips
-    county_code VARCHAR(50),												-- Mã quận nội bang (để thuận tiện tra cứu)
+    county_fips VARCHAR(5) NOT NULL,										-- Khóa ngoại tham chiếu tới bảng Counties qua county_fips
+    county_code VARCHAR(3),													-- Mã quận nội bang (để thuận tiện tra cứu)
     date DATE,																-- Ngày đo AQI
     aqi INT,																-- Chỉ số AQI
     category_id INT NOT NULL,												-- Khóa ngoại tham chiếu tới bảng Category
-    defining_parameter VARCHAR(50),											-- Thông số chính xác định AQI (VD: PM2.5, Ozone)
-    defining_site VARCHAR(50),												-- Tên trạm đo
+    defining_parameter VARCHAR(255),										-- Thông số chính xác định AQI (VD: PM2.5, Ozone)
+    defining_site VARCHAR(255),												-- Tên trạm đo
     num_sites_reporting INT,												-- Số lượng trạm báo cáo dữ liệu												-- ID nguồn gốc của dữ liệu
     created_date DATETIME DEFAULT GETDATE(),								-- Ngày tạo bản ghi
     updated_date DATETIME DEFAULT GETDATE(),								-- Ngày cập nhật bản ghi
-    FOREIGN KEY (county_fips) REFERENCES NDSDB.dbo.Counties(county_fips), 	-- Ràng buộc khóa ngoại tới bảng Counties
+    FOREIGN KEY (county_fips) REFERENCES NDSDB.dbo.County(county_fips), 	-- Ràng buộc khóa ngoại tới bảng Counties
     FOREIGN KEY (category_id) REFERENCES NDSDB.dbo.Category(category_id)	-- Ràng buộc khóa ngoại tới bảng Category
 );
 GO
 
 
-INSERT INTO NDSDB.dbo.Category (category_name, LowerBound, UpperBound, aqi_color, description)
+INSERT INTO NDSDB.dbo.Category (category_name, lower_bound, upper_bound, aqi_color, description)
 VALUES
 ('Good', 0, 50, 'Green', 'Air quality is satisfactory, and air pollution poses little or no risk.'),
 ('Moderate', 51, 100, 'Yellow', 'Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.'),
--- Script/stage.sql
+++ Script/stage.sql
@@ -5,29 +5,29 @@ USE StageDB;
 
 -- Bảng Counties
 CREATE TABLE StageDB.dbo.Counties (
-    county VARCHAR(50),                          -- Tên quận
-    county_ascii VARCHAR(50),                    -- Tên quận không dấu
-    county_full VARCHAR(100),                    -- Tên đầy đủ của quận
-    county_fips VARCHAR(50) PRIMARY KEY,         -- Mã định danh duy nhất của quận (FIPS code)
-    state_id VARCHAR(50),                        -- Mã viết tắt của bang, độ dài cố định là 2 ký tự
-    state_name VARCHAR(50),                      -- Tên bang
-    lat FLOAT,                                   -- Vĩ độ của quận
-    lng FLOAT,                                   -- Kinh độ của quận
-    population INT                               -- Dân số của quận
+    county VARCHAR(255),                        -- Tên quận
+    county_ascii VARCHAR(255),                  -- Tên quận không dấu
+    county_full VARCHAR(255),                   -- Tên đầy đủ của quận
+    county_fips VARCHAR(5) PRIMARY KEY,			-- Mã định danh duy nhất của quận (FIPS code)
+    state_abbr VARCHAR(2),						-- Mã viết tắt của bang, độ dài cố định là 2 ký tự
+    state_name VARCHAR(255),					-- Tên bang
+    lat decimal(9, 6),                          -- Vĩ độ của quận
+    lng decimal(9, 6),                          -- Kinh độ của quận
+    population INT                              -- Dân số của quận
 );
 
 
 -- Bảng AQI_Measurements 
 CREATE TABLE StageDB.dbo.AQI_Measurements (
-    state_name VARCHAR(50),											-- Tên bang nơi đo lường AQI (ví dụ: "California")
-    county_name VARCHAR(50),										-- Tên quận nơi đo lường AQI (ví dụ: "Los Angeles")
-    state_code VARCHAR(50),											-- Mã định danh bang (ví dụ: 6 cho California)
-    county_code VARCHAR(50),										-- Mã quận nội bang (để thuận tiện tra cứu)
+    state_name VARCHAR(255),										-- Tên bang nơi đo lường AQI (ví dụ: "California")
+    county_name VARCHAR(255),										-- Tên quận nơi đo lường AQI (ví dụ: "Los Angeles")
+    state_code VARCHAR(2),											-- Mã định danh bang (ví dụ: 6 cho California)
+    county_code VARCHAR(3),											-- Mã quận nội bang (để thuận tiện tra cứu)
     date DATE,														-- Ngày đo lường AQI
     aqi INT,														-- Chỉ số AQI (Air Quality Index)
-    category VARCHAR(50),											-- Loại chất lượng không khí (ví dụ: "Good", "Moderate", v.v.)
-    defining_parameter VARCHAR(50),									-- Thông số chính xác định AQI (ví dụ: "PM2.5", "Ozone")
-    defining_site VARCHAR(50),										-- Tên trạm đo lường AQI
+    category VARCHAR(255),											-- Loại chất lượng không khí (ví dụ: "Good", "Moderate", v.v.)
+    defining_parameter VARCHAR(255),								-- Thông số chính xác định AQI (ví dụ: "PM2.5", "Ozone")
+    defining_site VARCHAR(255),										-- Tên trạm đo lường AQI
     num_sites_reporting INT,										-- Số lượng trạm báo cáo dữ liệu
     created DATETIME,												-- Thời gian tạo bản ghi (mặc định là thời điểm hiện tại)
     last_updated DATETIME,											-- Thời gian cập nhật bản ghi (mặc định là thời điểm hiện tại)
