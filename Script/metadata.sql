45d2f5507923ce02773bf6fe714ef52bc7d0567c
BoundlessWay <70361753+BoundlessWay@users.noreply.github.com>
2024-12-29 23:03:58 +0700

    fix conflict

--- Script/metadata.sql
+++ Script/metadata.sql
@@ -13,17 +13,9 @@ CREATE TABLE ds_data_store (
 )
 GO
 
-CREATE TABLE ds_table_type (
-	tb_type_key INT PRIMARY KEY,
-	table_type VARCHAR(50),
-	description NVARCHAR(1000)
-)
-GO
-
 CREATE TABLE ds_table (
 	tb_key INT PRIMARY KEY,
 	tb_name VARCHAR(100),
-	table_type INT,
 	data_store INT,
 	description NVARCHAR(1000),
 
@@ -31,14 +23,11 @@ CREATE TABLE ds_table (
 	CONSTRAINT FK_table_datastore
 	FOREIGN KEY (data_store)
 	REFERENCES ds_data_store (ds_key),
-
-	CONSTRAINT FK_table_tabletype 
-	FOREIGN KEY (table_type)
-	REFERENCES ds_table_type (tb_type_key)
 )
 GO
 
-CREATE TABLE ds_column_type (
+CREATE TABLE ds_column_type
+(
 	column_type_key INT PRIMARY KEY,
 	column_type_name VARCHAR(50),
 	location VARCHAR(100),
@@ -46,7 +35,8 @@ CREATE TABLE ds_column_type (
 )
 GO
 
-CREATE TABLE ds_column (
+CREATE TABLE ds_column 
+(
 	column_key INT PRIMARY KEY,
 	table_key INT,
 	column_name VARCHAR(50),
@@ -61,7 +51,7 @@ CREATE TABLE ds_column (
 	REFERENCES ds_table (tb_key)
 )
 
-
+--ETL Process Metadata
 CREATE TABLE status (
     status_id INT PRIMARY KEY,
     status VARCHAR(50)
@@ -70,21 +60,70 @@ CREATE TABLE status (
 GO
 
 CREATE TABLE data_flow (
-    flow_id INT IDENTITY(1,1) PRIMARY KEY,
+    flow_id INT IDENTITY(1, 1) PRIMARY KEY,
     name VARCHAR(50),
     description VARCHAR(255),
-    source VARCHAR(50),
-    target VARCHAR(50),
-    transformation VARCHAR(255),
     status_id INT,
     LSET DATETIME,
     CET DATETIME,
     FOREIGN KEY (status_id) REFERENCES status(status_id)
 );
 
-
-
 --Audit Metadata
+--event category
+CREATE TABLE event_category
+(
+	id_category INT PRIMARY KEY,
+	event_category VARCHAR(255) NULL
+);
+--event type
+CREATE TABLE event_type
+(
+	id_type INT PRIMARY KEY,
+	event_type VARCHAR(255) NULL
+);
+--event_log
+CREATE TABLE event_log
+(
+	id_log INT PRIMARY KEY,
+	id_event_type INT NULL,
+	id_event_category INT NULL,
+	timestamp DATETIME NULL,
+	object INT NULL,
+	data_flow INT NULL,
+	rows INT NULL,
+	NOTE VARCHAR(255) NULL
+)
+GO
+
+--FOREIGN KEY
+--event_log to event_category
+ALTER TABLE event_log  WITH CHECK ADD CONSTRAINT [FK_event_category] FOREIGN KEY(id_event_category)
+REFERENCES event_category(id_category)
+GO
+ALTER TABLE event_log CHECK CONSTRAINT [FK_event_category]
+GO
+
+--event_log to event_type
+ALTER TABLE event_log  WITH CHECK ADD CONSTRAINT [FK_event_type] FOREIGN KEY(id_event_type)
+REFERENCES event_type(id_type)
+GO
+ALTER TABLE [dbo].[event_log] CHECK CONSTRAINT [FK_event_type]
+GO
+
+--event_log to ds_table
+ALTER TABLE event_log  WITH CHECK ADD CONSTRAINT [FK_event_table] FOREIGN KEY(object)
+REFERENCES ds_table(tb_key)
+GO
+ALTER TABLE event_log CHECK CONSTRAINT [FK_event_table]
+GO
+
+--event_log to data_flow
+ALTER TABLE event_log  WITH CHECK ADD CONSTRAINT [FK_event_data_flow] FOREIGN KEY(data_flow)
+REFERENCES data_flow(flow_id)
+GO
+ALTER TABLE event_log CHECK CONSTRAINT [FK_event_data_flow]
+GO
 --event_category
 SET ANSI_NULLS ON
 GO
